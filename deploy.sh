89d051670c36437c593cfbc141a05deeccb98414
sowani <sowani@gmail.com>
2015-04-24 06:01:07 -0400

    testing trigger

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
   echo -e "Starting to update demo\n"
 
   #copy data we're interested in to other place
-  cp -R ../DemoRepo $HOME/DemoRepo
+  #cp -R ../DemoRepo $HOME/DemoRepo
 
   #go to home and setup git
   cd $HOME
@@ -13,16 +13,16 @@
   git config --global user.name "sowani"
 
   #using token clone gh-pages branch
-  git clone --quiet https://${GH_TOKEN}@github.com/sowani/DemoRepo.git
+  #git clone --quiet https://${GH_TOKEN}@github.com/sowani/DemoRepo.git
 
   #go into diractory and copy data we're interested in to that directory
   cd DemoRepo
-  cp -Rf $HOME/DemoRepo/* .
+  #cp -Rf $HOME/DemoRepo/* .
 
   #add, commit and push files
   git add -f .
   git commit -m "push demo"
-  git push -fq origin master
+  git push origin master
 
   echo -e "Done magic with coverage\n"
 
