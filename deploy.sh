791e1bd892f4f321fef129a90e470c21a74ffba8
sowani <sowani@gmail.com>
2015-04-24 04:41:57 -0400

    testing trigger

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 ssh -o StrictHostKeyChecking=no
-if [ "$TRAVIS_BRANCH" == "travis" ]; then
-    git add -f .
-    git commit -m "Add built output"
-    git push https://$(GH_USER):$(GH_PASSWORD)@github.com/sowani/DemoRepo.git} demo
-fi
+git add -f .
+git commit -m "Add built output"
+git push https://$(GH_USER):$(GH_PASSWORD)@github.com/sowani/DemoRepo.git demo
